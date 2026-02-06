95dd52e7be59bc4cc6b709afce05ffb2f3e29f0a
mr funny guy <reallylookin2022@outlook.com>
2026-02-06 15:00:52 -0700

    Create U1_L1_Exercise1.java

---
+++ Unit 1/Lesson 1/U1_L1_Exercise1.java
@@ -0,0 +1,10 @@
+public class U1_L1_Exercise1
+{
+  // The objective is to make a print(ln) statement that says "I made this!"
+  public static void main(String[] args)
+  {
+    // Write code after this line
+
+    // Write code before this line
+  }
+}
